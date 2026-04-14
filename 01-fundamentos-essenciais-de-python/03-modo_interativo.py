f5093e19d79572de474fbd86e9757e160770bc6a
Chrystine Martins <126017173+Chrysthy@users.noreply.github.com>
2026-04-14 16:10:03 -0300

    feat: add modo interativo

---
+++ 01-fundamentos-essenciais-de-python/03-modo_interativo.py
@@ -0,0 +1,24 @@
+# modo interativo = só escrever python no terminal ou python -i [nome do arquivo]
+# para sair escreva exit()
+# letra q para sair da documentação que abriu no help
+# para sair da documentação do help, aperta enter
+
+
+# Mostra tudo que está disponível no escopo atual (variáveis, funções, etc.)
+dir()
+
+# É útil pra:
+# descobrir funções disponíveis
+# explorar objetos
+
+
+# Mostra todos os métodos e atributos do número 100 (tipo int)
+dir(100)
+
+# Ou seja:
+# você está vendo tudo que um inteiro pode fazer
+
+
+# Abre o sistema de ajuda interativo do Python
+help()
+help(100)
