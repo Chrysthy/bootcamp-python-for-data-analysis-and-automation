9106c66b841ea6488c395aa51bcb48be40b90109
Chrystine Martins <126017173+Chrysthy@users.noreply.github.com>
2026-04-15 11:17:38 -0300

    feat: add Variáveis e constantes

---
+++ 01_fundamentos_essenciais_de_python/04_variaveis_e_constantes.py
@@ -0,0 +1,22 @@
+# variávies
+nome = "Chrystine"
+idade = 34
+print(nome, idade)
+
+# podemos declar em uma linha também, com ou sem parenteses
+# nome, idade = ("Leon", 4) 
+nome, idade = "Leon", 4
+print(nome, idade)
+
+nome = "Noob"
+idade = 16
+print(nome, idade)
+
+limite_saque_diario = 1000
+print(limite_saque_diario)
+limite_saque_diario = 900
+print(limite_saque_diario)
+
+
+# constante - não podem ser reatribuídas e é sempre com letra maiúscula
+BRAZILIAN_STATES = ["SP", "RJ", "SC", "RS"]
