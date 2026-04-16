24b539186e5b7ea57d8af65680b26460f67914ec
Chrystine Martins <126017173+Chrysthy@users.noreply.github.com>
2026-04-16 14:16:05 -0300

    feat: add Operadores de comparação

---
+++ 01_fundamentos_essenciais_de_python/08_operadores_de_comparacao.py
@@ -0,0 +1,12 @@
+# Operadores de comparação
+
+saldo = 450
+saque = 200
+
+print(saldo == saque) # False
+print(saldo != saque) # True
+
+print(saldo > saque) # True
+print(saldo >= saque) # True
+print(saldo < saque) # False
+print(saldo <= saque) # False
