f4b260bdc2929e0701a8381e59296b1f400ea3df
Chrystine Martins <126017173+Chrysthy@users.noreply.github.com>
2026-04-16 14:24:11 -0300

    feat: add Operadores de atribuição

---
+++ 01_fundamentos_essenciais_de_python/09_operadores_de_atribuicao.py
@@ -0,0 +1,42 @@
+# Operadores de atribuição
+# defini um valor inicial ou sobrescreve o valor de uma variável
+
+# atribuição simples
+saldo = 500
+print(saldo)
+
+# atribuição com adição
+saldo = 500
+saldo += 200
+# saldo = saldo + 200
+print(saldo)
+
+# atribuição com subtração
+saldo = 500
+saldo -= 200
+print(saldo)
+
+# atribuição com divisão float e inteira
+saldo = 500
+saldo /= 5
+print(saldo)
+
+saldo = 500
+saldo //= 5
+print(saldo)
+
+# atribuição com multiplicação
+saldo = 500
+saldo *= 5
+print(saldo)
+
+
+# atribuição com módulo - resto da divisão
+saldo = 500
+saldo %= 480
+print(saldo)
+
+# atribuição com exponenciação
+saldo = 80
+saldo **= 2
+print(saldo)
