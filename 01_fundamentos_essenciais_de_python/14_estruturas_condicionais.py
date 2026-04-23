901ff534bc5c1373fdda849dc5772c5bc284422c
Chrystine Martins <126017173+Chrysthy@users.noreply.github.com>
2026-04-23 14:22:48 -0300

    feat: add elif example

--- 01_fundamentos_essenciais_de_python/14_estruturas_condicionais.py
+++ 01_fundamentos_essenciais_de_python/14_estruturas_condicionais.py
@@ -28,3 +28,18 @@ else:
     print("Saldo insuficiente!")
 
 
+# elif
+# O elif (abreviação de else if) é usado quando existem múltiplas condições possíveis. Ele evita o uso de vários if independentes.
+opcao = int(input("Informe uma opção: [1] Sacar \n [2] Extrato"))
+
+if opcao == 1:
+    valor = float(input("Informe a quantidade para o saque:"))
+
+elif opcao == 2:
+    print("Exibindo o extrato...")
+
+else:
+    sys.exit("Opção inválida.")
+
+
+
