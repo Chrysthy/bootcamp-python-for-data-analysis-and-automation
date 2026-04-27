81c4640496d66617515f71b0d9afd94841f382b2
Chrystine Martins <126017173+Chrysthy@users.noreply.github.com>
2026-04-27 14:52:12 -0300

    feat: add comando while

--- 01_fundamentos_essenciais_de_python/15_estruturas_de_repeticao.py
+++ 01_fundamentos_essenciais_de_python/15_estruturas_de_repeticao.py
@@ -66,3 +66,25 @@ for numero in range(0, 51, 5):
 
 
 
+# Comando while
+# é usado para repetir um bloco de código várias vezes, mas faz sentido usar while quando não sabemos o número exato de vezes que o bloco de código deve ser executado
+opcao = -1
+
+while opcao != 0:
+    opcao = int(input("[1] Sacar \n[2] Extrato \n[0] Sair \n"))
+
+    if opcao == 1:
+        print("Sacando")
+
+    elif opcao == 2:
+        print("Exibindo extrato")
+
+else:
+    print("Obrigada por usar nosso sistema bancário, até logo!")
+
+
+
+
+
+
+
