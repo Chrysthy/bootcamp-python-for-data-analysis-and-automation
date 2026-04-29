d25e88477cc38475b5c62e8869768eafd8b57051
Chrystine Martins <126017173+Chrysthy@users.noreply.github.com>
2026-04-29 14:05:02 -0300

    feat: add método Maiúscula, minúscula e título

--- 01_fundamentos_essenciais_de_python/16_metodos_uteis_classe_string.py
+++ 01_fundamentos_essenciais_de_python/16_metodos_uteis_classe_string.py
@@ -1,2 +1,14 @@
 # Conhecendo métodos úteis da classe string
+# Os principais métodos para manipular objetivos do tipo string e interpolar valores de variáveis e entender como funciona o fatiamento
+
+# Maiúscula, minúscula e título
+curso = "pYtHon"
+
+print(curso.upper())   # Converte todo o texto da variável 'curso' para letras maiúsculas
+
+print(curso.lower())   # Converte todo o texto da variável 'curso' para letras minúsculas
+
+print(curso.title())   # Converte o texto para "Title Case", ou seja, cada palavra começa com letra maiúscula
+
+
 
