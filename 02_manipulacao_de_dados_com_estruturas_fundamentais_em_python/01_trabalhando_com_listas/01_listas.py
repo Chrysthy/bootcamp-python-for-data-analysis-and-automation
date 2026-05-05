4fa017c6fca94aa33bb6ad1ecb767180ebc3b378
Chrystine Martins <126017173+Chrysthy@users.noreply.github.com>
2026-05-05 14:42:48 -0300

    feat: add Listas aninhadas

--- 02_manipulacao_de_dados_com_estruturas_fundamentais_em_python/01_trabalhando_com_listas/01_listas.py
+++ 02_manipulacao_de_dados_com_estruturas_fundamentais_em_python/01_trabalhando_com_listas/01_listas.py
@@ -55,3 +55,19 @@ print(frutas[-1]) # pera
 print(frutas[-3]) # laranja
 
 
+# ===============================================================================
+
+# Listas aninhadas
+# Listas podem armazenar todos os tipos de objetos Python, portanto podemos ter listas que armazenam outras listas. Com isso podemos criar estruturas bidimensionais 
+# (tabelas), e acessar informando os índices de linha e coluna.
+
+matriz = [
+    [1, "a", 2],
+    ["b", 3, 4],
+    [6, 5, "c"]
+]
+
+matriz[0]       # [1, "a", 2]
+matriz[0][0]    # 1
+matriz[0][-1]   # 2
+matriz[-1][-1]  # "c"
