0176c6d70f0a7c2564198a4be7ddc56d8444d398
Chrystine Martins <126017173+Chrysthy@users.noreply.github.com>
2026-05-05 14:41:58 -0300

    feat: add formas de declarar uma lista

---
+++ 02_manipulacao_de_dados_com_estruturas_fundamentais_em_python/01_trabalhando_com_listas/01_listas.py
@@ -0,0 +1,38 @@
+# Trabalhando com Listas em Python
+# Criação e acesso aos dados
+
+# Listas em Python podem armazenar de maneira sequencial qualquer tipo de objeto. Podemos criar listas utilizando o construtor list, a função range ou colocando valores 
+# separados por vírgula dentro de colchetes. Listas são objetos mutáveis, portanto podemos alterar seus valores após a criação.
+
+# Em Python, tudo é objeto.
+
+# forma mais comum de declarar uma lista
+frutas = ['laranja', 'maça', 'uva']
+
+frutas = [] # lista vazia
+
+
+letras = list('python')
+# A função list() pega cada caractere da string e transforma em elemento da lista:
+# ['p', 'y', 't', 'h', 'o', 'n']
+
+
+numeros = list(range(10))
+# range(10) gera números de 0 até 9
+# list() transforma isso em lista
+# [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+
+
+carro = ['Ferrari', 'F8', 4_200_000.50, 2020, 2900, 'São Paulo', True]
+# Lista com tipos mistos
+# Tipos dentro da sua lista:
+# 'Ferrari' → str
+# 'F8' → str
+# 4_200_000.50 → float (decimal)
+# 2020 → int
+# 2900 → int
+# 'São Paulo' → str
+# True → bool
+
+
