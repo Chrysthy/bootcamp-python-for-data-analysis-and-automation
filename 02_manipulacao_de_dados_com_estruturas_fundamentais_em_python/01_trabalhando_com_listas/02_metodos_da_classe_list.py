1b2f73bbe7764e774162d8fa093ef1f70e588e29
Chrystine Martins <126017173+Chrysthy@users.noreply.github.com>
2026-05-08 13:47:20 -0300

    feat: add sorted method

--- 02_manipulacao_de_dados_com_estruturas_fundamentais_em_python/01_trabalhando_com_listas/02_metodos_da_classe_list.py
+++ 02_manipulacao_de_dados_com_estruturas_fundamentais_em_python/01_trabalhando_com_listas/02_metodos_da_classe_list.py
@@ -173,5 +173,20 @@ palavra = "python"
 print(len(palavra))  # A string "python" tem 6 caracteres.
 
 
+# é uma função embutida que retorna o tamanho de uma sequência ou coleção, ou seja, quantos elementos ela contém.
 
-é uma função embutida que retorna o tamanho de uma sequência ou coleção, ou seja, quantos elementos ela contém.
+
+# ====================================================================================================
+# .sorted
+linguagens = ["python", "js", "c", "java", "csharp"]
+
+sorted(linguagens, key=lambda x: len(x))   # ["c", "js", "java", "python", "csharp"]
+
+sorted(linguagens, key=lambda x: len(x), reverse=True)   # ["python", "csharp", "java", "js", "c"]
+
+
+# é usada para ordenar listas (ou outras coleções), mas diferente de .sort(), ela não altera a lista original — em vez disso, retorna uma nova lista ordenada.
+
+# key=lambda x: len(x) → define o critério de ordenação: o tamanho das palavras.
+# reverse=True → inverte a ordem, mostrando do maior para o menor.
+# sorted() → cria uma nova lista ordenada, mantendo a original intacta.
