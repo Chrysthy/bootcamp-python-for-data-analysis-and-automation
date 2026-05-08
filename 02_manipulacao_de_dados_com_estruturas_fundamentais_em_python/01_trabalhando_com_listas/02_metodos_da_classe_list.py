0e5fe811b06d7a77539f22dac2448a6f44063081
Chrystine Martins <126017173+Chrysthy@users.noreply.github.com>
2026-05-08 13:37:53 -0300

    feat: add len method

--- 02_manipulacao_de_dados_com_estruturas_fundamentais_em_python/01_trabalhando_com_listas/02_metodos_da_classe_list.py
+++ 02_manipulacao_de_dados_com_estruturas_fundamentais_em_python/01_trabalhando_com_listas/02_metodos_da_classe_list.py
@@ -161,3 +161,17 @@ nova_lista = sorted(linguagens)
 
 # len(x) retorna o tamanho (número de caracteres) da string x.
 # Ordene a lista usando o comprimento das palavras como critério.
+
+
+# ====================================================================================================
+# .len
+linguagens = ["python", "js", "c", "java", "csharp"]
+
+len(linguagens) #  A lista tem 5 elementos, então o resultado é 5
+
+palavra = "python"
+print(len(palavra))  # A string "python" tem 6 caracteres.
+
+
+
+é uma função embutida que retorna o tamanho de uma sequência ou coleção, ou seja, quantos elementos ela contém.
