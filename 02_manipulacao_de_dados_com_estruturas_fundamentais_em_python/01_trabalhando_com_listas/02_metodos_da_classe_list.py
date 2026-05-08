943e82072726b58790d2ec3e34bd9c3cf2a609f3
Chrystine Martins <126017173+Chrysthy@users.noreply.github.com>
2026-05-08 11:24:31 -0300

    feat: add index method

--- 02_manipulacao_de_dados_com_estruturas_fundamentais_em_python/01_trabalhando_com_listas/02_metodos_da_classe_list.py
+++ 02_manipulacao_de_dados_com_estruturas_fundamentais_em_python/01_trabalhando_com_listas/02_metodos_da_classe_list.py
@@ -81,3 +81,14 @@ print(linguagens)                    # imprime a lista atualizada -> ['python', 
 # Sintaxe:
 # lista.extend([elemento1, elemento2, ...])
 
+
+# ====================================================================================================
+# .index
+linguagens = ['python', 'js', 'c', 'java', 'csharp']
+
+linguagens.index('java') #3
+linguagens.index('python') #0
+
+ 
+# Serve para localizar a posição de um elemento dentro de uma lista. Ele retorna o índice da primeira ocorrência encontrada.
+# Quando existem elementos repetidos, .index() não mostra todos os índices, apenas o primeiro.
