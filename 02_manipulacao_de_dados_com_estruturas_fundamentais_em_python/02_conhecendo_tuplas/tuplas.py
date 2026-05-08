b897f42118ed9c38e6ff3919a963f10bc4fa9102
Chrystine Martins <126017173+Chrysthy@users.noreply.github.com>
2026-05-08 16:05:19 -0300

    feat: add acesso direto as tuplas

--- 02_manipulacao_de_dados_com_estruturas_fundamentais_em_python/02_conhecendo_tuplas/tuplas.py
+++ 02_manipulacao_de_dados_com_estruturas_fundamentais_em_python/02_conhecendo_tuplas/tuplas.py
@@ -17,4 +17,17 @@ pais = ('Brasil')
 pais = ('Brasil',)
 
 
+# Acesso direto 
+# A tupla é uma sequência, portanto podemos acessar seus dados utilizando índices. Contamos o índice de determinada sequência a partir do zero.
+# Tuplas permitem acesso direto, mas não permitem alteração dos valores — são imutáveis.
 
+frutas = ('maça', 'laranja', 'uva', 'pera',)
+
+frutas[0]
+frutas[2]
+
+
+frutas = ('maça', 'laranja', 'uva', 'pera',)
+
+frutas[-1]
+frutas[-3]
