a02380f0a70d7fff24161a1f14942074d806d562
Chrystine Martins <126017173+Chrysthy@users.noreply.github.com>
2026-05-08 11:29:32 -0300

    feat: add pop method

--- 02_manipulacao_de_dados_com_estruturas_fundamentais_em_python/01_trabalhando_com_listas/02_metodos_da_classe_list.py
+++ 02_manipulacao_de_dados_com_estruturas_fundamentais_em_python/01_trabalhando_com_listas/02_metodos_da_classe_list.py
@@ -92,3 +92,17 @@ linguagens.index('python') #0
  
 # Serve para localizar a posição de um elemento dentro de uma lista. Ele retorna o índice da primeira ocorrência encontrada.
 # Quando existem elementos repetidos, .index() não mostra todos os índices, apenas o primeiro.
+
+
+# ====================================================================================================
+# .pop
+linguagens = ['python', 'js', 'c', 'java', 'csharp']
+
+linguagens.pop()    # remove 'csharp'
+linguagens.pop()    # remove 'java'
+linguagens.pop()    # remove 'c'
+linguagens.pop(0)   # remove 'python'
+
+
+# é usado para remover elementos. Por padrão, ele remove o último item da lista, mas você também pode passar um índice específico para remover outro elemento.
+# Se você tentar remover um índice que não existe, o Python gera um IndexError.
