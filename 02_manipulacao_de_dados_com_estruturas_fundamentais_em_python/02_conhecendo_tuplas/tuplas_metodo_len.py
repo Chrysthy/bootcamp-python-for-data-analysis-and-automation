dcada8e4313135132817b191d38dc89571477fb0
Chrystine Martins <126017173+Chrysthy@users.noreply.github.com>
2026-05-11 16:01:39 -0300

    feat: add método len

---
+++ 02_manipulacao_de_dados_com_estruturas_fundamentais_em_python/02_conhecendo_tuplas/tuplas_metodo_len.py
@@ -0,0 +1,12 @@
+# len
+
+linguagens = ("python", "js", "c", "java", "csharp",)
+
+len(linguagens) # 5
+
+# A função len() retorna o tamanho da sequência, ou seja, quantos itens ela contém.
+
+# No caso, a tupla linguagens possui 5 elementos, então o resultado é 5.
+
+# 🔹 Observação:  
+# O len() funciona da mesma forma para listas, strings e dicionários — sempre retornando a quantidade de itens ou caracteres.
