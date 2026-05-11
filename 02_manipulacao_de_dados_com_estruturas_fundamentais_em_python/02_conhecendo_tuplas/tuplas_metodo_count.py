e6a39c033ea29f4754c566ac778567f950ba32cd
Chrystine Martins <126017173+Chrysthy@users.noreply.github.com>
2026-05-11 16:00:07 -0300

    feat: add método count

---
+++ 02_manipulacao_de_dados_com_estruturas_fundamentais_em_python/02_conhecendo_tuplas/tuplas_metodo_count.py
@@ -0,0 +1,15 @@
+# count
+
+cores = ('vermelho', 'azul', 'verde', 'azul',)
+
+cores.count('vermelho') # 1
+cores.count('azul') # 2
+cores.count('verde') # 1
+
+# tupla.count(valor) retorna o número de vezes que o valor aparece dentro da tupla.
+
+# Tuplas são imutáveis, ou seja, você não pode adicionar ou remover elementos depois de criadas.
+
+# Listas são mutáveis, permitindo alterações.
+
+# Mas métodos como .count() e .index() funcionam igual em ambos os tipos.
