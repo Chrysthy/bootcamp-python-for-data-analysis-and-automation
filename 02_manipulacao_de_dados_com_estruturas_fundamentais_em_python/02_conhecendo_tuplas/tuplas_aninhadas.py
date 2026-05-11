cd8a9c94c2956e6d55e913b503e63f7641e33358
Chrystine Martins <126017173+Chrysthy@users.noreply.github.com>
2026-05-11 10:53:52 -0300

    feat: add tuplas aninhadas

---
+++ 02_manipulacao_de_dados_com_estruturas_fundamentais_em_python/02_conhecendo_tuplas/tuplas_aninhadas.py
@@ -0,0 +1,14 @@
+# Tuplas aninhadas  
+# Tuplas podem armazenar todos os tipos de objetos Python, portanto podemos ter tuplas que armazenam outras tuplas. 
+# Com isso podemos criar estruturas bidimensionais (tabelas), e acessar informando os índices de linha e coluna.
+
+matriz = (
+    (1, "a", 2),
+    ("b", 3, 4),
+    (6, 5, "c"),
+)
+
+matriz[0]      # (1, "a", 2)
+matriz[0][0]   # 1
+matriz[0][-1]  # 2
+matriz[-1][-1] # "c"
