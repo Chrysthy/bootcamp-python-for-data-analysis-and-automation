a171d5c885a4c477b4fd82867cf581963090bb53
Chrystine Martins <126017173+Chrysthy@users.noreply.github.com>
2026-05-11 16:00:15 -0300

    feat: add metodo index

---
+++ 02_manipulacao_de_dados_com_estruturas_fundamentais_em_python/02_conhecendo_tuplas/tuplas_metodo_index.py
@@ -0,0 +1,13 @@
+# index
+
+linguagens = ("python", "js", "c", "java", "csharp",)
+
+linguagens.index("java")   # 3
+linguagens.index("python") # 0
+
+
+# Ele retorna o índice da primeira ocorrência do valor informado.
+
+# Assim como o método .count(), o .index() funciona igual em tuplas e listas.
+
+# A diferença é que, se o valor não existir, o Python gera um erro ValueError.
