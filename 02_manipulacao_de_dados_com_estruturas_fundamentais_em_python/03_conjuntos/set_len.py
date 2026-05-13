9469741d4d2feaa51ab81a8940b81947a9392bb4
Chrystine Martins <126017173+Chrysthy@users.noreply.github.com>
2026-05-13 14:31:51 -0300

    feat: add len method

---
+++ 02_manipulacao_de_dados_com_estruturas_fundamentais_em_python/03_conjuntos/set_len.py
@@ -0,0 +1,13 @@
+# len
+
+numeros = {1, 2, 3, 1, 2, 4, 5, 5, 6, 7, 8, 9, 0}
+
+len(numeros) # 10
+
+# retorna a quantidade de elementos dentro de um conjunto.
+
+# O conjunto elimina automaticamente duplicatas.
+
+# Então, mesmo que você tenha repetido 1, 2 e 5 na declaração, eles aparecem apenas uma vez.
+
+# O conjunto final é: {0, 1, 2, 3, 4, 5, 6, 7, 8, 9} → exatamente 10 elementos únicos.
