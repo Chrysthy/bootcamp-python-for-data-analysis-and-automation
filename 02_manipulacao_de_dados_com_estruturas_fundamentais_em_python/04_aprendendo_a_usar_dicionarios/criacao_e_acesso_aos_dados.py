50c5933f6590698405e8830bcb5904e4da579db0
Chrystine Martins <126017173+Chrysthy@users.noreply.github.com>
2026-05-13 15:32:22 -0300

    feat: add definição de dicionário

---
+++ 02_manipulacao_de_dados_com_estruturas_fundamentais_em_python/04_aprendendo_a_usar_dicionarios/criacao_e_acesso_aos_dados.py
@@ -0,0 +1,4 @@
+# Aprendendo a Utilizar Dicionários em Python
+
+# Um dicionário é um conjunto não-ordenado de pares chave:valor, onde as chaves são únicas em uma dada instância do dicionário.
+# Dicionários são delimitados por chaves {}, e contêm uma lista de pares chave:valor separada por vírgulas.
