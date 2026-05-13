f1f16d32ca064699763753da60ea092ae916e009
Chrystine Martins <126017173+Chrysthy@users.noreply.github.com>
2026-05-13 13:33:26 -0300

    feat: add clear method

---
+++ 02_manipulacao_de_dados_com_estruturas_fundamentais_em_python/03_conjuntos/set_clear.py
@@ -0,0 +1,17 @@
+# clear
+
+sorteio = {1, 23}
+
+print(sorteio) # {1, 23}
+
+sorteio.clear()
+
+print(sorteio) # {}
+
+# serve para esvaziar completamente um conjunto, removendo todos os elementos de uma vez.
+
+# Antes do clear, o conjunto tinha {1, 23}.
+
+# Depois do clear, o conjunto fica vazio.
+
+# O resultado é set(), que representa um conjunto vazio (não confundir com {}, que seria um dicionário vazio).
