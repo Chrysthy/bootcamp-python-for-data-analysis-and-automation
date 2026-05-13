baccd029c5347c1984c61b96ec9bae0b16f0a66a
Chrystine Martins <126017173+Chrysthy@users.noreply.github.com>
2026-05-13 14:32:01 -0300

    feat: add in method

---
+++ 02_manipulacao_de_dados_com_estruturas_fundamentais_em_python/03_conjuntos/set_in.py
@@ -0,0 +1,18 @@
+# in
+
+numeros = {1, 2, 3, 1, 2, 4, 5, 5, 6, 7, 8, 9, 0}
+
+1 in numeros # True
+10 in numeros # False
+
+
+# serve para verificar se um elemento está presente dentro de um conjunto (ou de outras coleções como listas, tuplas, strings, etc.).
+
+# O conjunto elimina duplicatas automaticamente, então numeros é na verdade {0, 1, 2, 3, 4, 5, 6, 7, 8, 9}.
+
+# O 1 está presente → retorna True.
+
+# O 10 não está presente → retorna False.
+
+# Observações
+# in → verifica se o elemento existe no conjunto.
