4a37bf26412f9f0348e18debbcce3ed6e3f17c07
Chrystine Martins <126017173+Chrysthy@users.noreply.github.com>
2026-05-13 11:22:21 -0300

    feat: add example

--- 02_manipulacao_de_dados_com_estruturas_fundamentais_em_python/03_conjuntos/set_iterar_conjuntos.py
+++ 02_manipulacao_de_dados_com_estruturas_fundamentais_em_python/03_conjuntos/set_iterar_conjuntos.py
@@ -1,2 +1,11 @@
 # Iterar conjuntos
 # A forma mais comum para percorrer os dados de um conjunto é utilizando o comando for
+
+carros = {"gol", "celta", "palio"}
+
+for carro in carros:
+    print(carro)
+
+
+# O loop vai imprimir cada elemento do conjunto.
+# Mas atenção: como os sets não têm ordem fixa, a saída pode variar — às vezes começa pelo "gol", outras pelo "palio". O importante é que todos os elementos aparecem uma vez.
