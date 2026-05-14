169b52da27c95e3e7d7c7321f9fdeead091c7fbf
Chrystine Martins <126017173+Chrysthy@users.noreply.github.com>
2026-05-14 16:07:01 -0300

    feat: add exemplo dicionário

--- 02_manipulacao_de_dados_com_estruturas_fundamentais_em_python/04_aprendendo_a_usar_dicionarios/criacao_e_acesso_aos_dados.py
+++ 02_manipulacao_de_dados_com_estruturas_fundamentais_em_python/04_aprendendo_a_usar_dicionarios/criacao_e_acesso_aos_dados.py
@@ -2,3 +2,9 @@
 
 # Um dicionário é um conjunto não-ordenado de pares chave:valor, onde as chaves são únicas em uma dada instância do dicionário.
 # Dicionários são delimitados por chaves {}, e contêm uma lista de pares chave:valor separada por vírgulas.
+
+pessoa = {"nome": "Chrystine", "idade": 34}
+
+pessoa = dict(nome="Chrystine", idade=34)
+
+pessoa["telefone"] = "3333-1234"
