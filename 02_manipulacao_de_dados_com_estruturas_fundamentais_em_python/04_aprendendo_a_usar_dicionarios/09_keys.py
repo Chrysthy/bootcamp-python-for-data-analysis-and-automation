a65238595f8e9feb1cca737d7a5a33a66f805495
Chrystine Martins <126017173+Chrysthy@users.noreply.github.com>
2026-05-20 16:05:44 -0300

    feat: add keys method

---
+++ 02_manipulacao_de_dados_com_estruturas_fundamentais_em_python/04_aprendendo_a_usar_dicionarios/09_keys.py
@@ -0,0 +1,20 @@
+# Keys
+
+contatos = {
+    "guilherme@gmail.com": {"nome": "Guilherme", "telefone": "3333-2221"}
+}
+
+contatos.keys()
+# dict_keys(['guilherme@gmail.com'])
+# Aqui, o dicionário tem apenas uma chave: "guilherme@gmail.com".
+# O resultado mostra isso dentro de dict_keys.
+
+
+# Retorna todas as chaves do dicionário em um objeto especial chamado dict_keys.
+
+novo_dicionario = {"a": 100, 1: "teste", "b": "python"}
+print(novo_dicionario.keys())
+# dict_keys(['a', 1, 'b'])
+
+
+# Saber todas as chaves que um dicionário tem
