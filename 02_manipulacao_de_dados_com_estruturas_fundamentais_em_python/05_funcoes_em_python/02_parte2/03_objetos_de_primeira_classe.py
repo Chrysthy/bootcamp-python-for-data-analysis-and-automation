0cbd225c838401b0c39db726fdd92cfd4c608ffb
Chrystine Martins <126017173+Chrysthy@users.noreply.github.com>
2026-06-19 15:45:21 -0300

    feat: add Objetos de primeira classe

---
+++ 02_manipulacao_de_dados_com_estruturas_fundamentais_em_python/05_funcoes_em_python/02_parte2/03_objetos_de_primeira_classe.py
@@ -0,0 +1,26 @@
+# Objetos de primeira classe
+
+# Em Python tudo é objeto, dessa forma funções também são objetos, o que as tornam objetos de primeira classe.
+# Com isso podemos atribuir funções a variáveis, passá-las como parâmetro para funções, usá-las como valores em estruturas de dados (listas, tuplas, dicionários, etc.) e 
+# usá-las como valor de retorno para uma função (closures).
+
+def somar(a, b):
+    return a + b
+
+
+def subtrair(a, b):
+    return a - b
+
+
+def test(a, b):
+    return a * 2 + b *3
+
+
+def exibir_resultado(a, b, funcao):
+    resultado = funcao(a, b)
+    print(f"O resultado da operação {a} + {b} = {resultado}")
+
+exibir_resultado(10, 10, somar)  # O resultado da operação 10 + 10 = 20
+exibir_resultado(10, 10, subtrair) # O resultado da operação 10 + 10 = 0
+exibir_resultado(10, 10, test) # O resultado da operação 10 + 10 = 50  
+# (porque 10 × 2 + 10  × 3 = 20 + 30 = 50)
