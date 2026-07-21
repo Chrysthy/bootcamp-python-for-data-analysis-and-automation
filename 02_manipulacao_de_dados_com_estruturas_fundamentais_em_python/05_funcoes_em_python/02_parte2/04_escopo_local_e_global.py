ce3cb3c7c4fde6f3388c94defdc585083db49e0e
Chrystine Martins <126017173+Chrysthy@users.noreply.github.com>
2026-07-21 15:00:02 -0300

    feat: add Escopo local e escopo global

---
+++ 02_manipulacao_de_dados_com_estruturas_fundamentais_em_python/05_funcoes_em_python/02_parte2/04_escopo_local_e_global.py
@@ -0,0 +1,6 @@
+# Escopo local e escopo global
+
+# Python trabalha com escopo local e global, dentro do bloco da função o escopo é local. 
+# Portanto, alterações ali feitas em objetos imutáveis serão perdidas quando o método terminar de ser executado. 
+# Para usar objetos globais utilizamos a palavra‑chave global, que informa ao interpretador que a variável que está sendo manipulada no escopo local é global.
+# Essa NÃO é uma boa prática e deve ser evitada.
