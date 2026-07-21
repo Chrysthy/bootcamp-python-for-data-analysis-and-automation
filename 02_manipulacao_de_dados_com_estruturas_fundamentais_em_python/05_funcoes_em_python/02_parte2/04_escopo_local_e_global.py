a79f797bdea22fe549ee0ca9b88461188ee1ca72
Chrystine Martins <126017173+Chrysthy@users.noreply.github.com>
2026-07-21 15:04:57 -0300

    feat: add exemplo escopo global

--- 02_manipulacao_de_dados_com_estruturas_fundamentais_em_python/05_funcoes_em_python/02_parte2/04_escopo_local_e_global.py
+++ 02_manipulacao_de_dados_com_estruturas_fundamentais_em_python/05_funcoes_em_python/02_parte2/04_escopo_local_e_global.py
@@ -4,3 +4,12 @@
 # Portanto, alterações ali feitas em objetos imutáveis serão perdidas quando o método terminar de ser executado. 
 # Para usar objetos globais utilizamos a palavra‑chave global, que informa ao interpretador que a variável que está sendo manipulada no escopo local é global.
 # Essa NÃO é uma boa prática e deve ser evitada.
+
+salario = 2000 # variável com escopo global, está fora da função
+
+def salario_bonus(bonus):
+    global salario # global para informar que estamos pegando a variável de escopo global
+    salario += bonus
+    return salario
+
+salario_bonus(500) # 2500
