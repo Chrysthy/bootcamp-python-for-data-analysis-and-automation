7678eb5585f0f1d8c3833abc7f0478616253fa1d
Chrystine Martins <126017173+Chrysthy@users.noreply.github.com>
2026-09-22 14:03:42 -0300

    feat: add exemplo introdução, coleta de dados e integrações

---
+++ 10_python_aplicado_a_analises/01_automacao_de_processos_e_analises/01_introducao/script.py
@@ -0,0 +1,18 @@
+import csv
+
+total = 0
+
+with open("vendas.txt","w") as f:
+    f.write("100\n200\n150\n300")
+
+
+
+
+
+with open("vendas.txt","r") as f:
+
+    for linha in f:
+        valor = int(linha.strip())
+        total += valor
+
+print("Total de vendas:", total)
